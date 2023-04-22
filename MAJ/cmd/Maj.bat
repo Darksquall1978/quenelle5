951b55f2f41f6b4a0ce703f8a185f5b796ae9901
Darksquall1978 <93328208+Darksquall1978@users.noreply.github.com>
2023-04-22 12:32:45 +0200

    Add files via upload

---
+++ MAJ/cmd/Maj.bat
@@ -0,0 +1,22 @@
+@echo off
+Color 0e & Mode con cols=100 lines=40
+
+IF EXIST "Maj GIT".bat del "Maj GIT".bat
+
+git pull
+
+::@echo Mise a jour OK
+timeout /t 3 /nobreak > nul
+
+::RMDIR /s /q quenelle5
+
+::modif.bat
+
+cd..
+cd..
+::RMDIR /s /q Git
+
+start Launcher.exe
+exit
+
+
